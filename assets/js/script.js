c9794ca0c540680428837b1e8def163ff72c6856
NarminRahimova <nemorahimova11@gmail.com>
2024-02-12 15:06:07 +0400

    completed array-methods

---
+++ assets/js/script.js
@@ -0,0 +1,102 @@
+let fruits=["Grapefruit","Lemon","Orange","Mango","Apple","Cherry"];
+// console.log(fruits.length); // pop 
+// fruits.pop();
+// fruits.pop();
+// console.log(fruits);
+// fruits.push("Banana","Avocado"); // push 
+// console.log(fruits);
+
+// fruits.shift();
+// fruits.shift();
+// console.log(fruits);  // shift 
+
+// fruits.unshift("Strawberry");
+// console.log(fruits);  // unshift 
+
+
+// console.log(fruits.join (" +1 ")); // join
+
+// console.log(fruits.toString()); // toString 
+
+// let plusNumbers=[2,5,8,10,13];
+// let minusNumbers=[-3,-5,-9,-15,-22];
+// console.log(plusNumbers.concat(minusNumbers));  //concat 
+
+// delete plusNumbers[4];
+// console.log(plusNumbers[4]);  // delete
+// console.log(fruits.slice(2,4));  // slice
+
+// fruits.splice(1,2, "Watermelon");
+// console.log(fruits);  // splice
+
+// const fruit = fruits.at(2);
+// console.log(fruit);  // at
+
+
+// let numbers=[10,20,30,40,50];
+// numbers.copyWithin(4);
+// console.log(numbers); // copyWithin
+
+//Task-1
+// function bolme (){
+//         let arr=[123,99,6,12,43,79,102,23,35,57];
+//         let sum=0;
+//         for(let i=0; i<arr.length;i++){
+//             if(arr[i]<100 && arr[i]%3==0){
+//                 sum+=arr[i];
+//             }
+//         }
+//         console.log(sum);
+//      }
+//      bolme();
+
+     //! Task-2
+//     function Say(){
+//     let arr = [123,99,6,12,43,79,102,23,35,57];
+//     let count=0;
+//     let edediOrta=0;
+//     let sum=0;
+//     let maxEded=0;
+//     let minEded=Infinity;
+//     let sumMaxandMin=0;
+//    for (let i = 0; i < arr.length; i++) {
+//   sum+=arr[i];
+//   edediOrta=sum/arr.length;
+//   if(maxEded<arr[i]){
+//     maxEded=arr[i];
+//   }
+//   if(minEded>arr[i]){
+//     minEded=arr[i];
+//   }
+// }
+// console.log(sum)
+// console.log(edediOrta)
+// sumMaxandMin=minEded+maxEded;
+// console.log(sumMaxandMin)
+
+// if(edediOrta>sumMaxandMin){
+//   console.log("Ededi orta boyukdur...")
+// }else{
+//   console.log("Ededi orta kicikdir...")
+// }
+// }
+// Say();
+
+// //? Task-3
+// function numbers(){
+//   let arr = [1, 23, 43, -34, 45, -12, -43, 54,-44];
+//   let minusCount = 0;
+//   let plusCount=0;
+// for (let i = 0; i < arr.length; i++) {
+//    if (arr[i] < 0) {
+//      minusCount++;
+
+//    } else {
+//      plusCount++;
+//    }
+//  }
+//   console.log( "musbet ededlerin sayi : "+plusCount);
+//  console.log( "menfi ededlerin sayi : "+minusCount);
+// }
+// numbers();
+    
